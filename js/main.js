af17da9ed17067338a85142609e2e7fdf1ee9f31
berthakuo <berthakuo@gmail.com>
2014-04-19 09:59:32 -0400

    trying geolocation

--- js/main.js
+++ js/main.js
@@ -221,38 +221,56 @@ var map;
 
 /*function loadCambridge (){*/
 
-var cambridge = new google.maps.LatLng(42.364081,-71.101503);
-
-var mapOptions = {
-	center: cambridge,
-	zoom: 16
-};
-
-map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-var styles = [
+var x = document.getElementById("demo");
+function getLocation()
   {
-    stylers: [
-      { hue: "#110d24" },
-      { saturation: -100 }
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      { lightness: 100 },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{
+  	console.log("Geolocation is not supported by this browser.")}
   }
-];
+function showPosition(position){
+  var lat = position.coords.latitude;
+  var lng =position.coords.longitude; 
+  
 
-map.setOptions({styles: styles});	
+
+
+	var here = new google.maps.LatLng(lat,lng);
+	var cambridge = new google.maps.LatLng(42.364081,-71.101503);
+
+	var mapOptions = {
+		center: here,
+		zoom: 16
+	};
+
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	var styles = [
+	  {
+	    stylers: [
+	      { hue: "#110d24" },
+	      { saturation: -100 }
+	    ]
+	  },{
+	    featureType: "road",
+	    elementType: "geometry",
+	    stylers: [
+	      { lightness: 100 },
+	      { visibility: "simplified" }
+	    ]
+	  },{
+	    featureType: "road",
+	    elementType: "labels",
+	    stylers: [
+	      { visibility: "off" }
+	    ]
+	  }
+	];
+
+	map.setOptions({styles: styles});	
+}
 /********************************
 Clear markers and SET UP SEARCH ARRAY
 **********************************/
